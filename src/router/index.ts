7394b8d254dc745e3fd92680316a827c5e83e873
umurakbulut <akbulutumur@gmail.com>
2026-02-05 23:08:55 +0300

    feat: initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const HomeView = () => import('@/views/HomeView.vue')
+
+const routes = [{ path: '/', name: 'Home', component: HomeView }]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
